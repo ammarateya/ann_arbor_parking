d51883056b8d18626b662350fe3bd5ce090e3a13
ammarateya <ammarat@umich.edu>
2025-10-29 15:37:06 -0400

    scraper: default range to ±50 around AA/NC seeds for faster runs; update env template example

--- scraper_only.py
+++ scraper_only.py
@@ -80,7 +80,7 @@ def ongoing_scraper_job():
       - North Campus seed: NC_BASE_SEED env var (defaults to 2081673)
 
     Range size:
-      - SCRAPE_RANGE_SIZE env var (defaults to 200)
+      - SCRAPE_RANGE_SIZE env var (defaults to 50)
     """
     logger.info("Starting ongoing scraper job...")
     
@@ -131,9 +131,9 @@ def ongoing_scraper_job():
         except ValueError:
             nc_seed = 2081673
         try:
-            range_size = int(os.getenv('SCRAPE_RANGE_SIZE', '200'))
+            range_size = int(os.getenv('SCRAPE_RANGE_SIZE', '50'))
         except ValueError:
-            range_size = 200
+            range_size = 50
 
         aa_range = (aa_seed - range_size, aa_seed + range_size)
         nc_range = (nc_seed - range_size, nc_seed + range_size)
